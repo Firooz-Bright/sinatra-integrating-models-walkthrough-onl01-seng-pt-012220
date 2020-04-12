910a0a7b7a4d9d3cdd0d1fa25113a62861467f48
Firooz Mafakheri <firooz.mafakheri@gmail.com>
2020-04-12 11:06:03 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ class App < Sinatra::Base
 
    post '/' do
     
-    analyzed_text = TextAnalyzer.new(params[:user_text])
+    @analyzed_text = TextAnalyzer.new(params[:user_text])
  
    erb :results
  end 
